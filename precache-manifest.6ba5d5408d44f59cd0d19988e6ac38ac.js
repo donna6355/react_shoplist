1878b2ec00413d531dc62538d1e19a4575489db9
donna6355 <donnajun1214@gmail.com>
2020-10-23 15:03:33 +0900

    Updates

---
+++ precache-manifest.6ba5d5408d44f59cd0d19988e6ac38ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3221ffa2507b79cbcbd21d4cc666ec4b",
+    "url": "/react_shoplist/index.html"
+  },
+  {
+    "revision": "912965fba43f1f51ae2c",
+    "url": "/react_shoplist/static/css/main.44233662.chunk.css"
+  },
+  {
+    "revision": "8fa08eefe7a06414b194",
+    "url": "/react_shoplist/static/js/2.2c24b15c.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react_shoplist/static/js/2.2c24b15c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "912965fba43f1f51ae2c",
+    "url": "/react_shoplist/static/js/main.2e81b381.chunk.js"
+  },
+  {
+    "revision": "76487b944ffc5959ba63",
+    "url": "/react_shoplist/static/js/runtime-main.71ab3ae7.js"
+  },
+  {
+    "revision": "7d6ed49107de3aead217cff0d9029603",
+    "url": "/react_shoplist/static/media/react_kermit.7d6ed491.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react_shoplist/precache-manifest.6ba5d5408d44f59cd0d19988e6ac38ac.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_shoplist/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
